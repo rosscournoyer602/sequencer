c03265562eaecaba8cc832597f5b60c7f195dbec
Ross Cournoyer <rcoupvc@yahoo.com>
2020-05-12 13:29:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 const port = process.env.PORT || 80;
 app.listen(port);
